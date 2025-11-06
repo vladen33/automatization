c95a797e835417992a5c8fb9845845d8fa5bd47f
Vladislav Vasilev <vladen33@gmail.com>
2025-11-06 14:46:08 +0300

    Add gulp config

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat-css');
+const plumber = require('gulp-plumber');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
