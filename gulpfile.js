1440fe1a0536dcd63f4c6dff6e4a8c5b6ed5a370
Vasiliev Vladislav <vladen33@gmail.com>
2025-11-06 20:33:00 +0300

    Update gulpconf

--- gulpfile.js
+++ gulpfile.js
@@ -24,8 +24,24 @@ function images() {
         .pipe(gulp.dest('dist/images'))
 }
 
+function clean() {
+    return gulp.del('dist');
+}
 
+const build = gulp.series(clean, gulp.parallel(html, css, images));
+
+function watchFiles() {
+    gulp.watch(['src/**/*.html'], html);
+    gulp.watch(['src/blocks/**/*.css'], css);
+    gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+}
+
+const watchapp = gulp.parallel(build, watchFiles);
 
 exports.html = html;
 exports.css = css;
 exports.images = images;
+exports.clean = clean;
+
+exports.build = build;
+exports.watchapp = watchapp;
