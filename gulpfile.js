ea5ad252123600e529c113b887f5c63276f01b32
Vladislav Vasilev <vladen33@gmail.com>
2025-11-06 15:02:09 +0300

    Update gulpconf.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,12 @@ const plumber = require('gulp-plumber');
 const del = require('del');
 const browserSync = require('browser-sync').create();
 
+
+function html() {
+    return gulp.src('src/**/*.html')
+        .pipe(plumber())
+        .pipe(gulp.dest('dist/'))
+}
+
+
+exports.html = html;
